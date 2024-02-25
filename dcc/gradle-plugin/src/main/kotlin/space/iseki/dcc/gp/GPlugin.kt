f7796937d345e30309ffd0093ac47236766abc82
iseki <admin@iseki.space>
2024-02-25 16:24:01 +0800

    style: suppress

--- dcc/gradle-plugin/src/main/kotlin/space/iseki/dcc/gp/GPlugin.kt
+++ dcc/gradle-plugin/src/main/kotlin/space/iseki/dcc/gp/GPlugin.kt
@@ -5,17 +5,19 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.compile.AbstractCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import space.iseki.dcc.gen.PostProcess
+import java.io.File
 
+@Suppress("unused")
 class GPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        val LOG = target.logger
+        @Suppress("LocalVariableName") val LOG = target.logger
         LOG.info("DCC plugin apply")
         target.afterEvaluate { project ->
             for (task in project.tasks.filter { it is AbstractCompile || it is KotlinCompilationTask<*> }) {
                 LOG.info("register to task: {}", task)
                 task.doLast {
                     LOG.info("begin post-process for task: {}", task)
-                    for (dir in it.outputs.files.files.filter { it.isDirectory }) {
+                    for (dir in it.outputs.files.files.filter(File::isDirectory)) {
                         LOG.info("post-processing dir: {}", dir)
                         PostProcess.processDir(dir)
                     }
