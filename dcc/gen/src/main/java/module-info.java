6ef7646cd112242da9148b71fbc153377952a19a
iseki <admin@iseki.space>
2024-03-05 11:25:00 +0800

    fix: JPMS

--- dcc/gen/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module space.iseki.dcc.gen {
-    requires kotlin.stdlib;
-    requires static org.jetbrains.annotations;
-    requires space.iseki.dcc.api;
-    requires org.objectweb.asm;
-    requires org.objectweb.asm.util;
-}
