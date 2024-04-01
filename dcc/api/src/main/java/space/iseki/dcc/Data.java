b87bf8b9a4778d9a7cca739dcb3338162da0b5f6
iseki <admin@iseki.space>
2024-04-01 22:25:31 +0800

    fix: override IDEA default analysis on Data.getCodec

--- dcc/api/src/main/java/space/iseki/dcc/Data.java
+++ dcc/api/src/main/java/space/iseki/dcc/Data.java
@@ -1,9 +1,12 @@
 package space.iseki.dcc;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public class Data {
+    @SuppressWarnings("Contract")
+    @Contract("!null -> !null; null -> fail") // override the default analyze
     public static <T> @NotNull Codec<T> getCodec(@NotNull Class<T> type) {
         throw new AssertionError("intrinsic");
     }
